b79bb26add2517bf51aab2f34cceef62354ec404
25HP7048 25HP7048 <25HP7048@G305-employeeiMac.localdomain>
2025-07-29 15:00:56 +0800

    Update code with latest changes

--- dom/script.js
+++
@@ -1,6 +0,0 @@
-const mainContainer = document.getElementById("main");
-mainContainer.className = "main";
-const text = document.createElement("p");
-const note = document.createTextNode("hello world");
-text.appendChild(note);
-document.getElementById("main").appendChild(text);
